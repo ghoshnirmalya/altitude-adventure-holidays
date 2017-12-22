125e43313c067e3635fcfa6b7343dc943c11a2f9
ghoshnirmalya <nirmalya.email@gmail.com>
2017-12-22 13:09:49 +0530

    feat(navbar): make the navbar toggle work using Reactstrap

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,33 +1,52 @@
 import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem
+} from 'reactstrap';
 import Link from 'gatsby-link';
 
 import './styles.scss';
 
 import BlackLogo from '../../assets/images/logo-black.png';
 
-const Header = () => {
-  return (
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link to="/" className="navbar-brand">
-          <img src={BlackLogo} width="30" height="30" alt="" />
-        </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/blogs" className="nav-link">Blogs</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
-}
+export default class Header extends React.Component {
+  constructor() {
+    super();
 
-export default Header;
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Navbar color="faded" light expand="md">
+          <Link to="/" className="navbar-brand">
+            <img src={BlackLogo} width="30" height="30" alt="" />
+          </Link>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link to="/" className="nav-link">Home</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/blogs" className="nav-link">Blogs</Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
